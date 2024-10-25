4d0bdaf6da368f5470c99f0be99bbb7bd9dbd3cd
Amin Lotfian <166643821+Amin-Lotfian@users.noreply.github.com>
2024-10-25 16:21:41 +0200

    Update 0-sqlite basic queris- beginners.sql

--- 0-sqlite basic queris- beginners.sql
+++ 0-sqlite basic queris- beginners.sql
@@ -7,4 +7,36 @@ GROUP BY
 HAVING
 ORDER BY
 LIMIT
+
+ORDER OF EXECUTION:
+FROM/JOIN
+WHERE
+GROUP BY
+HAVING
+SELECT
+DISTINCT
+ORDER BY
+LIMIT/OFFSET
 */
+
+
+QUERIES:
+1.
+SELECT job_postings_fact.job_title_short, 
+  	   job_postings_fact.job_location
+FROM job_postings_fact
+LIMIT 7
+
+2.
+SELECT DISTINCT job_postings_fact.job_title_short 			
+FROM job_postings_fact;
+SELECT DISTINCT job_postings_fact.job_location
+FROM job_postings_fact;
+
+3.	
+SELECT DISTINCT job_postings_fact.job_title_short, 
+            				job_postings_fact.job_location
+
+FROM job_postings_fact;
+
+
